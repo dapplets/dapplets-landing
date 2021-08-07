44c8eae37c6ce1f3259f24545dee35d9a15352c5
Deokti <reilgelern@mail.ru>
2021-08-07 16:44:35 +0300

    feat: added slider and adaptiv for it

---
+++ src/assets/js/script.js
@@ -0,0 +1,16 @@
+const slider = tns({
+	container: '.slider-wrapper',
+	items: 1,
+
+	controls: false,
+	nav: false,
+	mouseDrag: true,
+});
+
+sliderButton('.slider-left', 'prev');
+sliderButton('.slider-right', 'next');
+
+function sliderButton(buttonSelector, direction) {
+	const button = document.querySelector(buttonSelector);
+	button.addEventListener('click', () => slider.goTo(direction));
+}
