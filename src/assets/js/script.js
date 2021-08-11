da3d4c016b912a856fe1345341b7f5989cd6fb8f
Deokti <reilgelern@mail.ru>
2021-08-11 17:36:58 +0300

    feat: added smooth scroll for click on links

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,3 +1,4 @@
+// Slider
 const slider = tns({
 	container: '.slider-wrapper',
 	items: 1,
@@ -14,4 +15,23 @@ sliderButton('.slider-right', 'next');
 function sliderButton(buttonSelector, direction) {
 	const button = document.querySelector(buttonSelector);
 	button.addEventListener('click', () => slider.goTo(direction));
+}
+
+// Smooth scroll
+document.querySelectorAll('a[href="#form"').forEach(smoothScrollOnElements);
+
+function smoothScrollOnElements(links) {
+	links.addEventListener('click', smoothScroll)
+}
+
+function smoothScroll(event) {
+	event.preventDefault()
+
+	const href = event.target.getAttribute('href').substr(1)
+
+	document.getElementById(href)
+		.scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		})
 }
